cd565fa09e20d05c7557d6c10a41b92639a5a17f
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2022-05-20 22:13:54 +0900

    バージョンを 0.2.0 に更新した。

--- examples/ipmi.rs
+++ examples/ipmi.rs
@@ -22,7 +22,7 @@ fn main() -> Result<(), Error> {
 
 fn cmd_ipmi() -> Command<'static> {
     Command::new("IPMI")
-        .version("0.1.0")
+        .version("0.2.0")
         .subcommand(cmd_chassis())
 }
 
